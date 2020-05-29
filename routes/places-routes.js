e40250b94f8de536218e5a29451b358fb6708e5c
Peter <pdnguyen714@gmail.com>
2020-05-29 16:10:20 -0700

    Implemented googleapi geocoding and validated patch and user routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -10,20 +10,29 @@ router.get('/:pid', placesControllers.getPlaceById);
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
 router.post(
-    '/', 
-    [
-        check('title')
-            .not()
-            .isEmpty(),
-        check('description').isLength({min: 5}),
-        check('address')
-            .not()
-            .isEmpty()
-    ],
-    placesControllers.createPlace
+  '/',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty()
+  ],
+  placesControllers.createPlace
 );
 
-router.patch('/:pid', placesControllers.updatePlace);
+router.patch(
+  '/:pid',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
+  placesControllers.updatePlace
+);
 
 router.delete('/:pid', placesControllers.deletePlace);
 
