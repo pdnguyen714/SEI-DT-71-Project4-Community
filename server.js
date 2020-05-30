1323877c9829aaf4c6b03945cff02f49704a239b
Peter <pdnguyen714@gmail.com>
2020-05-30 14:26:13 -0700

    Successfully established mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -25,4 +26,11 @@ app.use((error, req, res, next) => {
   res.json({message: error.message || 'An unknown error occurred!'});
 });
 
-app.listen(5000);
+mongoose
+.connect('mongodb+srv://sei:sei@cluster0-xtnng.mongodb.net/community?retryWrites=true&w=majority')
+.then(() => {
+  app.listen(5000);
+})
+.catch(err => {
+  console.log(err);
+});
