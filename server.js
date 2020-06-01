0d231b867a37151213ee0e3a213125e42e4f5bd1
Peter <pdnguyen714@gmail.com>
2020-06-01 16:27:14 -0700

    Proxy server test

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const fs = require('fs');
 
@@ -15,9 +14,6 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-
-app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
-app.use(express.static(path.join(__dirname, "build")));
 
 app.use(bodyParser.json());
 
