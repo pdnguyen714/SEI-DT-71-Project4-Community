e707bd08348f39c8e91e93c8e7b73685e27b8a20
Peter <pdnguyen714@gmail.com>
2020-06-08 15:10:34 -0700

    Changed single quotes to backticks in req routes

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -90,7 +90,7 @@ const Auth = () => {
         formData.append('password', formState.inputs.password.value);
         formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest(
-          '/api/users/signup',
+          `/api/users/signup`,
           'POST',
           formData
         );
