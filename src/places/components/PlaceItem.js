9d258ae447f94fc85d534fe163fc6967000ff6b3
Peter <pdnguyen714@gmail.com>
2020-06-08 13:56:08 -0700

    Refactored server side routing

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -84,7 +84,7 @@ const PlaceItem = props => {
           {isLoading && <LoadingSpinner asOverlay />}
           <div className="place-item__image">
             <img
-              src={`http://localhost:5000/${props.image}`}
+              src={props.image}
               alt={props.title}
             />
           </div>
